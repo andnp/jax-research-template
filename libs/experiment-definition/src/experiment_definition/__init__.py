4123abc95b5e1f557a0341d478c4e7441224497a
Andy Patterson <andnpatterson@gmail.com>
2026-09-03 11:39:32 -0600

    feat(experiment-definition): export the public builder API
    The documented import raises ImportError today.

--- libs/experiment-definition/src/experiment_definition/__init__.py
+++ libs/experiment-definition/src/experiment_definition/__init__.py
@@ -1,1 +1,19 @@
 """Public API for the experiment-definition library."""
+
+from __future__ import annotations
+
+from .bridge import metric_whitelist
+from .component import Component, ComponentType
+from .experiment import Experiment
+from .metric import MetricFrequency, MetricType
+from .parameter import ParameterValue
+
+__all__ = [
+    "Component",
+    "ComponentType",
+    "Experiment",
+    "MetricFrequency",
+    "MetricType",
+    "ParameterValue",
+    "metric_whitelist",
+]
